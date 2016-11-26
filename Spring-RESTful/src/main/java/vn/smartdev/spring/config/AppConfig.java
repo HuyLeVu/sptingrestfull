6bf9068d8941d2a208b2c151f2ab740d65fdb09b
huy <huylv.itedu@gmail.com>
2016-11-26 18:30:00 +0700

    spring restfull

---
+++ Spring-RESTful/src/main/java/vn/smartdev/spring/config/AppConfig.java
@@ -0,0 +1,12 @@
+package vn.smartdev.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="vn.smartdev")
+public class AppConfig {
+
+}
---
+++ Spring-RESTful/src/main/java/vn/smartdev/spring/config/AppInitializer.java
@@ -0,0 +1,27 @@
+package vn.smartdev.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class[]{AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[]{"/"};
+	}
+	//http://viralpatel.net/blogs/spring-4-mvc-rest-example-json/
+	
+
+}
